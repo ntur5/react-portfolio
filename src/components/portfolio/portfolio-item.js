46b976a7eb9141ca75a6dc76261be58f574ebfa7
ntur5 <pressntr@gmail.com>
2020-01-07 16:33:28 -0700

    rendered images from api

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,9 +2,11 @@ import React from "react"
 import { Link } from 'react-router-dom'
 
 export default function(props) {
-    const { id, description, thumb_image_url, logo_url } = props.item
+    const { id, description, thumb_image_url, logo } = props.item
     return (
         <div>
+            <img src={thumb_image_url} />
+            <img src={logo} />
             <div>{description}</div>
             <Link to={`/portfolio/${id}`}>Link</Link>
         </div>
