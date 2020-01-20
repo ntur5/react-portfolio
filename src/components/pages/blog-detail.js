6fa5080fb4b86a42636cd2b17bb91eb79c225f1f
pingt9*t <pressntr@gmail.com>
2020-01-19 21:58:10 -0700

    added a click handler to render the blog form when clicking blog title

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import axios from "axios"
 import ReactHtmlParse from "react-html-parser"
+
 import BlogFeaturedImage from "../blog/blog-featured-image"
+import BlogForm from '../blog/blog-form';
 
 export default class BlogDetail extends Component {
     constructor(props) {
         super(props)
         this.state = {
             currentId: this.props.match.params.slug,
-            blogItem: {}
+            blogItem: {},
+            editMode: false
         }
+
+        this.handleEditClick = this.handleEditClick.bind(this)
+    }
+
+    handleEditClick() {
+        console.log("handle edit clicked")
+        this.setState({ editMode: true })
     }
 
     getBlogItem() {
@@ -37,18 +47,29 @@ export default class BlogDetail extends Component {
             featured_image_url,
             blog_status
         } = this.state.blogItem
+
+        const contentManager = () => {
+            if (this.state.editMode) {
+                return <BlogForm />
+            } else {
+                return (
+                    <div className="content-container">
+                        <h1 onClick={this.handleEditClick}>{title}</h1>
+
+                        <BlogFeaturedImage img={featured_image_url} />
+
+                        <div className="content">
+                            {ReactHtmlParse(content)}
+                        </div>
+                        
+                    </div>
+                )
+            }
+        }
+
         return (
             <div className="blog-container">
-                <div className="content-container">
-                    <h1>{title}</h1>
-
-                    <BlogFeaturedImage img={featured_image_url} />
-
-                    <div className="content">
-                        {ReactHtmlParse(content)}
-                    </div>
-                    
-                </div>
+                {contentManager()}
             </div>
         )
     }
